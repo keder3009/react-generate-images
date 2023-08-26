5a2e756554551a4c7b3a2979e8dd12939b3c7aaf
k.munoz <k.munoz@superpagos.com>
2023-08-26 12:16:39 -0500

    aprendiendo el funcionamiento de los eventos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [linea1, setLinea1] = useState("");
+  const [linea2, setLinea2] = useState("");
+
+  const onChangeLinea1 = (event) => {
+    setLinea1(event.target.value);
+  };
+  const onChangeLinea2 = (event) => {
+    setLinea2(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select>
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History</option>
+        <option value="matrix">Matrix</option>
+        <option value="pholosoractor">Philosoractor</option>
+        <option value="smart">Smart Guy</option>
+      </select>{" "}
+      <br />
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1" />{" "}
+      <br />
+      <input onChange={onChangeLinea2} type="text" placeholder="Linea 2" />{" "}
+      <br />
+      <div>
+        <span>{linea1}</span> <br/>
+        <span>{linea2}</span>
+        {/* <img src="" /> */}
+      </div>
     </div>
   );
 }
