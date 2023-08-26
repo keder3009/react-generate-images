d6578badbad70d24e6cf4908348be6979f5eb50c
k.munoz <k.munoz@superpagos.com>
2023-08-26 12:28:05 -0500

    prueba de reactividad

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   const [linea1, setLinea1] = useState("");
   const [linea2, setLinea2] = useState("");
+  const [image, setImage] = useState("");
 
   const onChangeLinea1 = (event) => {
     setLinea1(event.target.value);
@@ -11,15 +12,18 @@ function App() {
   const onChangeLinea2 = (event) => {
     setLinea2(event.target.value);
   };
+  const onChangeImage = (event) => {
+    setImage(event.target.value);
+  };
 
   return (
     <div className="App">
-      <select>
+      <select onChange={onChangeImage}>
         <option value="fire">Casa en llamas</option>
         <option value="futurama">Futurama</option>
         <option value="history">History</option>
         <option value="matrix">Matrix</option>
-        <option value="pholosoractor">Philosoractor</option>
+        <option value="philosoraptor">Philosoraptor</option>
         <option value="smart">Smart Guy</option>
       </select>{" "}
       <br />
@@ -27,10 +31,10 @@ function App() {
       <br />
       <input onChange={onChangeLinea2} type="text" placeholder="Linea 2" />{" "}
       <br />
-      <div>
-        <span>{linea1}</span> <br/>
+      <div className="meme">
+        <span>{linea1}</span> <br />
         <span>{linea2}</span>
-        {/* <img src="" /> */}
+        <img src={`/img/${image}.jpg`} alt={image} />
       </div>
     </div>
   );
