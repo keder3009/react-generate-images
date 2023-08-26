08bf5022a6558c9b8b12ed0d59b8ef5fbd1fa064
k.munoz <k.munoz@superpagos.com>
2023-08-26 12:42:31 -0500

    metodo para descargar las imagenes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import html2canvas from 'html2canvas';
 import { useState } from "react";
 import "./App.css";
 
@@ -16,6 +17,16 @@ function App() {
     setImage(event.target.value);
   };
 
+  const onClickExportar = function (evento) {
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      var img = canvas.toDataURL("image/png");
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+    });
+  }
+
   return (
     <div className="App">
       <select onChange={onChangeImage}>
@@ -31,7 +42,8 @@ function App() {
       <br />
       <input onChange={onChangeLinea2} type="text" placeholder="Linea 2" />{" "}
       <br />
-      <div className="meme">
+      <button onClick={onClickExportar}>Exportar</button>
+      <div className="meme" id='meme'>
         <span>{linea1}</span> <br />
         <span>{linea2}</span>
         <img src={`/img/${image}.jpg`} alt={image} />
